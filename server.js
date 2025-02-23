a30c9ddb6da49f8f926064c713576df9df2064e4
= <=>
2025-02-24 00:18:36 +0530

    check route added

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.get('/', (req, res) => {
   res.status(200).json('Call Recording Server is running.');
 });
 
+app.get('/check', (req, res) => {
+  res.status(200).json('Call Recording Server is running.');
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
